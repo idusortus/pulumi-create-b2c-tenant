56ae2c01951d2b85e833fbd234ea8cbed9bd2406
idus <idusortus@gmail.com>
2021-05-29 21:36:56 -0500

    init

---
+++ quickstart/B2CStack.cs
@@ -0,0 +1,26 @@
+using Pulumi;
+// using Pulumi.AzureNative.AzureActiveDirectory.V20190101Preview;
+using AzureNative = Pulumi.AzureNative;
+
+class B2CStack : Stack
+{
+    public B2CStack()
+    {
+        var b2cTenant = new AzureNative.AzureActiveDirectory.B2CTenant("b2cTenant", new AzureNative.AzureActiveDirectory.B2CTenantArgs
+        {
+            Location = "United States",
+            Properties = new AzureNative.AzureActiveDirectory.Inputs.CreateTenantRequestBodyPropertiesArgs
+            {
+                CountryCode = "US",
+                DisplayName = "MatchHub",
+            },
+            ResourceGroupName = "fs-uuid-DEV",
+            ResourceName = "MatchHub.onmicrosoft.com",
+            Sku = new AzureNative.AzureActiveDirectory.Inputs.B2CResourceSKUArgs
+            {
+                Name = AzureNative.AzureActiveDirectory.B2CResourceSKUName.Standard,
+                Tier = AzureNative.AzureActiveDirectory.B2CResourceSKUTier.A0,
+            },
+        });
+    }
+}
